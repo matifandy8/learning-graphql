5c18292eb23166bdaf15eb5425ee1a3b098a7a16
matifandy <matifandy@gmail.com>
2021-02-13 16:10:11 -0300

    first commit

---
+++ src/components/CharacterCard.js
@@ -0,0 +1,10 @@
+const CharacterCard = ({ character }) => {
+  return (
+    <div className="character-card">
+      <img src={character.image} alt={character.name} />
+      <p>{character.name}</p>
+    </div>
+  );
+};
+
+export default CharacterCard;
